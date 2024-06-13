8b74c3483d6cb8bf0bf95bd6b5ad2948b8d22272
Victor Ferreira <victorferreira9990@gmail.com>
2024-06-13 06:59:42 -0300

    controllers

---
+++ controllers/arduinoController.js
@@ -0,0 +1,63 @@
+const { arduinoFacade } = require('../dependency/injection');
+
+const getArduinoById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const arduino = await arduinoFacade.getById(id);
+        if (!arduino) {
+            res.status(404).json({ message: 'Arduino não encontrado' });
+        } else {
+            res.json(arduino);
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getAllArduinos = async (req, res) => {
+    try {
+        const arduinos = await arduinoFacade.getAll();
+        res.json(arduinos);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const addArduino = async (req, res) => {
+    try {
+        const data = req.body;
+        const novoArduino = await arduinoFacade.add(data);
+        res.status(201).json(novoArduino);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const updateArduino = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const newData = req.body;
+        await arduinoFacade.update(id, newData);
+        res.status(200).json({ message: 'Arduino atualizado com sucesso' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const deleteArduino = async (req, res) => {
+    try {
+        const id = req.params.id;
+        await arduinoFacade.delete(id);
+        res.status(200).json({ message: 'Arduino deletado com sucesso' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = {
+    getArduinoById,
+    getAllArduinos,
+    addArduino,
+    updateArduino,
+    deleteArduino,
+};
