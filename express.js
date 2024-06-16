97dad424d94ecf69994c2b1fbc804149f4b3fa66
Victor Ferreira <victorferreira9990@gmail.com>
2024-06-15 22:06:50 -0300

    express

--- express.js
+++ express.js
@@ -2,27 +2,38 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-
 const authRoutes = require('./routes/authRoutes');
+const temperaturaRoutes = require('./routes/temperaturaRoutes');
+const phRoutes = require('./routes/phRoutes');
+const oxigenioRoutes = require('./routes/oxigenioRoutes');
 const criatorioRoutes = require('./routes/criatorioRoutes');
-const usuarioRoutes = require('./routes/usuarioRoutes');
-const peixeRoutes = require('./routes/peixeRoutes');
-const leituraRoutes = require('./routes/leituraRoutes');
 const arduinoRoutes = require('./routes/arduinoRoutes');
+const leituraRoutes = require('./routes/leituraRoutes');
+const peixeRoutes = require('./routes/peixeRoutes');
+const usuarioCriatorioRoutes = require('./routes/usuarioCriatorioRoutes');
+const usuarioRoutes = require('./routes/usuarioRoutes');
 
 const isAuth = require("./middleware/isAuth");
 
+// Middleware para JSON parsing
 app.use(express.json());
+
+// Middleware de autenticação (se necessário)
+// app.use(isAuth);
 
 // Rotas
 app.use("/api/auth", authRoutes);
+app.use("/api/temperatura", temperaturaRoutes);
+app.use("/api/ph", phRoutes);
+app.use("/api/oxigenio", oxigenioRoutes);
 app.use("/api/criatorio", criatorioRoutes);
-app.use("/api/usuario", usuarioRoutes);
-app.use("/api/peixe", peixeRoutes);
-app.use("/api/leitura", leituraRoutes);
 app.use("/api/arduino", arduinoRoutes);
+app.use("/api/leitura", leituraRoutes);
+app.use("/api/peixe", peixeRoutes);
+app.use("/api/usuarioCriatorio", usuarioCriatorioRoutes);
+app.use("/api/usuario", usuarioRoutes);
 
+// Inicialização do servidor
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
